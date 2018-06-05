1bde69a0c555e50fa1417318a7097d9473de9e30
vishal2082 <vishalbhardwaj070@gmail.com>
2018-06-05 11:37:55 +0530

    first commit

---
+++ src/main/java/com/codeship/HomeScreen.java
@@ -0,0 +1,10 @@
+package com.codeship;
+
+public class HomeScreen {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to Home sceen...");
+	}
+
+}
